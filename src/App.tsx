4766c6ab45607ef42188969c1833193cdd79b23c
HoangTC <hoang.tran@getz.co>
2022-04-04 18:20:43 +0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{process.env.name}</p>
+        <p>{process.env.BASE_URL}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
